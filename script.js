63522b3a46297764a60fb6b329f3b0a8b5dacf95
Robins <robinmgk077@gmail.com>
2023-12-10 11:41:45 +0300

    current update commit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function toggleMenu() {
   const hamburgerMenu = document.querySelector(".list");
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
   hamburgerMenu.classList.toggle("show");
+  hamburgerIcon.classList.toggle("active");
 }
 
 document.querySelectorAll(".items").forEach((item) => {
   item.addEventListener("click", () => {
     const hamburgerMenu = document.querySelector(".list");
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
     hamburgerMenu.classList.remove("show");
+    hamburgerIcon.classList.remove("active");
   });
 });
